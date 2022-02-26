9803b51ba726396d9d717b6963dea28cc764ef84
Dave <87390532+vadedes@users.noreply.github.com>
2022-02-27 00:32:24 +0800

    Added function to register user and save to database

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -4,6 +4,7 @@ import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRig
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { db } from '../firebase.config';
+import { doc, setDoc, serverTimestamp } from 'firebase/firestore';
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -38,6 +39,12 @@ function SignUp() {
       updateProfile(auth.currentUser, {
         displayName: name,
       });
+
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy);
 
       navigate('/');
     } catch (error) {
