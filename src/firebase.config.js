807c9fc943a98180c4d38ac41caf8e29034cfb54
Dave <87390532+vadedes@users.noreply.github.com>
2022-02-26 20:32:46 +0800

    Updated env file

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAe2iwFvq1qEcmrYLFmoH7Nbs8UivS73yo',
-  authDomain: 'house-marketplace-app-3b437.firebaseapp.com',
-  projectId: 'house-marketplace-app-3b437',
-  storageBucket: 'house-marketplace-app-3b437.appspot.com',
-  messagingSenderId: '207158933891',
-  appId: '1:207158933891:web:582ec9a0e54f6ba89e9eb6',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
