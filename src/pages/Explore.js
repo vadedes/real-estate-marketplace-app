37cea8300d37c37dd8a2583f8fe50688f87966cf
Dave <87390532+vadedes@users.noreply.github.com>
2022-02-28 21:19:46 +0800

    Implemented listings on explore page

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -18,8 +18,8 @@ function Explore() {
             <img src={rentCategoryImage} alt='rent' className='exploreCategoryImg' />
             <p className='exploreCategoryName'>Places for rent</p>
           </Link>
-          <Link to='/category/sell'>
-            <img src={sellCategoryImage} alt='sell' className='exploreCategoryImg' />
+          <Link to='/category/sale'>
+            <img src={sellCategoryImage} alt='sale' className='exploreCategoryImg' />
             <p className='exploreCategoryName'>Places for sale</p>
           </Link>
         </div>
