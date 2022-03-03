83ba22cb5b37dced1b201f5bc71ed9510165efa7
Dave <87390532+vadedes@users.noreply.github.com>
2022-03-03 19:24:36 +0800

    Built contact landlord page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Category from './pages/Category';
 import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
           <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
